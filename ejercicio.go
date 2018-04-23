09d86b2e5778255ac3a3eeeb7067d71036e31235
GAlejandro97 <registro_25@hotmail.es>
2018-04-22 21:04:18 -0500

    ejercicio 2

---
+++ ejercicio.go
@@ -0,0 +1,14 @@
+package main
+import (
+  "fmt"
+)
+
+func main() {
+var(
+  nameUser string ="Guillermo"
+  agentUser int = 21
+)
+
+fmt.Println("USUARIO : ",nameUser , "EDAD : ",agentUser )
+}
+//50 ejercicios de logica
